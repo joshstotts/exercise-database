7c85d6197d8f80d89c5ffd26f6e152e4b58af7d0
Josh Stotts <joshstotts@Joshs-MacBook-Pro.local>
2022-04-05 00:33:16 -0500

    added footer links

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,10 @@ function Footer() {
         <div>
             <footer className='navbar fixed-bottom bg-dark'>
                 <div id='copyright'>Exercise Database&copy; 2022</div>
-                <div><img id='bblogo' src="https://www.bodybuilding.com/images/icons/bbcom-b-logo-648.png" alt="bodybuilding.com logo" /></div>
+
+                <div><a href="https://www.nike.com/" target='_blank' rel='noreferrer'><img id='nikelogo' src="https://hellograds.com/wp-content/uploads/2020/02/Vis-ID-Nike-Swoosh.jpg" alt="nike logo" /></a></div>
+
+                <div><a href="https://www.bodybuilding.com/" target='_blank' rel='noreferrer'><img id='bblogo' src="https://www.bodybuilding.com/images/icons/bbcom-b-logo-648.png" alt="bodybuilding.com logo" /></a></div>
             </footer>
         </div>
     )
