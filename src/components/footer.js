b0e0d0db45159bbeb8d2a9688c5583bd299e3db7
Josh Stotts <joshstotts@Joshs-MacBook-Pro.local>
2022-04-05 22:02:38 -0500

    added more footer links

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,9 +7,25 @@ function Footer() {
             <footer className='navbar fixed-bottom bg-dark'>
                 <div id='copyright'>Exercise Database&copy; 2022</div>
 
-                <div><a href="https://www.nike.com/" target='_blank' rel='noreferrer'><img id='nikelogo' src="https://hellograds.com/wp-content/uploads/2020/02/Vis-ID-Nike-Swoosh.jpg" alt="nike logo" /></a></div>
+                <div><a href="https://www.spri.com/" target='_blank' rel='noreferrer'><img id='logo' src="https://cdn.shopify.com/s/files/1/1734/8163/files/SPRI-logo_205x_2x_7e15a0ac-cfcf-433e-9958-e29c606922c7.png?height=628&pad_color=ffffff&v=1487274627&width=1200" alt="SPRI logo" /></a></div>
 
-                <div><a href="https://www.bodybuilding.com/" target='_blank' rel='noreferrer'><img id='bblogo' src="https://www.bodybuilding.com/images/icons/bbcom-b-logo-648.png" alt="bodybuilding.com logo" /></a></div>
+                <div><a href="https://shop.lifefitness.com/" target='_blank' rel='noreferrer'><img id='logo' src="https://www.vector-logo.net/logo_preview/ai/l/LIFE_FITNESS_LOGO.png" alt="lifefitness logo" /></a></div>
+
+                <div><a href="https://www.tptherapy.com/" target='_blank' rel='noreferrer'><img id='logo' src="http://www.simplesoleproject.com/wp-content/uploads/2018/01/trigger-point-performance-therapy.png" alt="trigger point logo" /></a></div>
+
+                <div><a href="https://hyperice.com/" target='_blank' rel='noreferrer'><img id='logo' src="https://www.manua.ls/thumbs/brands/l/9619-hyperice_logo.jpg" alt="hyperic logo" /></a></div>
+
+                <div><a href="https://www.underarmour.com/en-us/" target='_blank' rel='noreferrer'><img id='logo' src="http://blog.logomyway.com/wp-content/uploads/2017/02/under-armour-logo-design.jpg" alt="Under armour logo" /></a></div>
+
+                <div><a href="https://www.adidas.com/us" target='_blank' rel='noreferrer'><img id='logo' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3yZXlQkARPHSnYvANn-IxPfjtFU_Szy9vxvDHeIPkWBL_R_54azhoJTzrj9oLB1IxCAM&usqp=CAU" alt="adidas logo" /></a></div>
+
+                <div><a href="https://shop.lululemon.com/" target='_blank' rel='noreferrer'><img id='logo' src="https://1000logos.net/wp-content/uploads/2017/08/Lululemon-emblem.jpg" alt="LuluLemon logo" /></a></div>
+
+                <div><a href="https://www.nike.com/" target='_blank' rel='noreferrer'><img id='logo' src="https://hellograds.com/wp-content/uploads/2020/02/Vis-ID-Nike-Swoosh.jpg" alt="nike logo" /></a></div>
+
+                <div><a href="https://www.bodybuilding.com/" target='_blank' rel='noreferrer'><img id='logo' src="https://www.bodybuilding.com/images/icons/bbcom-b-logo-648.png" alt="bodybuilding.com logo" /></a></div>
+
+
             </footer>
         </div>
     )
